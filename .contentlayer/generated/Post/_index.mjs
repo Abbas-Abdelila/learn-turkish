629863f9bbf50bb9d3eb848265e7dca6acb773ba
Abbas-Abdelila <abbasabdelila@gmail.com>
2023-08-19 04:37:09 +0300

    create: add yonelme eki article

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -6,5 +6,6 @@ import numbersMdx from './numbers.mdx.json' assert { type: 'json' }
 import pluralsMdx from './plurals.mdx.json' assert { type: 'json' }
 import thisThatHereThereMdx from './this-that-here-there.mdx.json' assert { type: 'json' }
 import varYokMdx from './var-yok.mdx.json' assert { type: 'json' }
+import yonelmeEkiToMdx from './yonelme-eki-to.mdx.json' assert { type: 'json' }
 
-export const allPosts = [getStartedArticleMdx, locativeSuffixMdx, numbersMdx, pluralsMdx, thisThatHereThereMdx, varYokMdx]
+export const allPosts = [getStartedArticleMdx, locativeSuffixMdx, numbersMdx, pluralsMdx, thisThatHereThereMdx, varYokMdx, yonelmeEkiToMdx]
