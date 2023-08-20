0cb89daf11e6e7dd1572141b8454e20c039ef4a0
Abbas-Abdelila <abbasabdelila@gmail.com>
2023-08-20 03:12:55 +0300

    create: from ayrilma hali eki article

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -7,5 +7,6 @@ import pluralsMdx from './plurals.mdx.json' assert { type: 'json' }
 import thisThatHereThereMdx from './this-that-here-there.mdx.json' assert { type: 'json' }
 import varYokMdx from './var-yok.mdx.json' assert { type: 'json' }
 import yonelmeEkiToMdx from './yonelme-eki-to.mdx.json' assert { type: 'json' }
+import fromDanDenTanTenMdx from './from-dan-den-tan-ten.mdx.json' assert { type: 'json' }
 
-export const allPosts = [getStartedArticleMdx, locativeSuffixMdx, numbersMdx, pluralsMdx, thisThatHereThereMdx, varYokMdx, yonelmeEkiToMdx]
+export const allPosts = [getStartedArticleMdx, locativeSuffixMdx, numbersMdx, pluralsMdx, thisThatHereThereMdx, varYokMdx, yonelmeEkiToMdx, fromDanDenTanTenMdx]
