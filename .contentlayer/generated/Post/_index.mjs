4d9e3827e3d82023a567f891951b7cd183632d57
Abbas-Abdelila <abbasabdelila@gmail.com>
2023-08-22 19:29:26 +0300

    edit: simple past belirtili article

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -6,9 +6,9 @@ import getStartedArticleMdx from './get-started-article.mdx.json' assert { type:
 import locativeSuffixMdx from './locative-suffix.mdx.json' assert { type: 'json' }
 import numbersMdx from './numbers.mdx.json' assert { type: 'json' }
 import pluralsMdx from './plurals.mdx.json' assert { type: 'json' }
+import simplePastBelirtiliMdx from './simple-past-belirtili.mdx.json' assert { type: 'json' }
 import thisThatHereThereMdx from './this-that-here-there.mdx.json' assert { type: 'json' }
 import varYokMdx from './var-yok.mdx.json' assert { type: 'json' }
 import yonelmeEkiToMdx from './yonelme-eki-to.mdx.json' assert { type: 'json' }
-import simplePastBelirtiliMdx from './simple-past-belirtili.mdx.json' assert { type: 'json' }
 
-export const allPosts = [belirtmeEkiTheMdx, fromDanDenTanTenMdx, getStartedArticleMdx, locativeSuffixMdx, numbersMdx, pluralsMdx, thisThatHereThereMdx, varYokMdx, yonelmeEkiToMdx, simplePastBelirtiliMdx]
+export const allPosts = [belirtmeEkiTheMdx, fromDanDenTanTenMdx, getStartedArticleMdx, locativeSuffixMdx, numbersMdx, pluralsMdx, simplePastBelirtiliMdx, thisThatHereThereMdx, varYokMdx, yonelmeEkiToMdx]
