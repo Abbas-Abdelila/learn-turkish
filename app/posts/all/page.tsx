97df4391c4b7649e2ae38a45d92259bbc2442cf7
Abbas-Abdelila <abbasabdelila@gmail.com>
2023-09-20 02:08:52 +0300

    add chat api route

--- app/posts/all/page.tsx
+++ app/posts/all/page.tsx
@@ -3,43 +3,47 @@ import { compareDesc, format, parseISO } from "date-fns";
 import { allPosts, Post } from "contentlayer/generated";
 // import { getMDXComponent } from "next-contentlayer/hooks";
 import Categories from "@/components/Categories";
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
+import Chat from "@/components/Chat";
 
 export const metadata: Metadata = {
-  title: 'All | Pick Turkish',
-  description: 'All posts of the blog of the website of Pick Turkish',
-}
-
-
+  title: "All | Pick Turkish",
+  description: "All posts of the blog of the website of Pick Turkish",
+};
 
 const Posts = () => {
-  const posts: Post[] = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
+  const posts: Post[] = allPosts.sort((a, b) =>
+    compareDesc(new Date(a.date), new Date(b.date))
+  );
   return (
     <div className="flex flex-col space-y-4 w-[95%] mx-auto">
-    <div className="flex my-10 justify-start">
-      <Categories selectedLevel="All" />
-    </div>
-    <div className="flex flex-col md:flex-row md:space-x-10">
-      <div className="md:w-[60%]">
-      {posts.map((post) => {
-        return (
-          <div key={post._id} className="flex flex-col mb-5 ">
-            <Link href={`${post.level.toLowerCase()}/${post.url}`}>
-            <h1 className="text-xl text-slate-800 font-medium hover:text-blue-700 hover:underline underline-[1px] decoration-red-200 underline-offset-[6px] cursor-pointer">{post.title}</h1>
-            </Link>
-            <time className="text-sm text-slate-700" >{format(parseISO(post.date), 'LLLL d, yyyy')}</time>
+      <div className="flex my-10 justify-start">
+        <Categories selectedLevel="All" />
+      </div>
+      <div className="flex flex-col md:flex-row md:space-x-10 divide-x">
+        <div className="md:w-[60%]">
+          {posts.map((post) => {
+            return (
+              <div key={post._id} className="flex flex-col mb-5 ">
+                <Link href={`${post.level.toLowerCase()}/${post.url}`}>
+                  <h1 className="text-xl text-slate-800 font-medium hover:text-blue-700 hover:underline underline-[1px] decoration-red-200 underline-offset-[6px] cursor-pointer">
+                    {post.title}
+                  </h1>
+                </Link>
+                <time className="text-sm text-slate-700">
+                  {format(parseISO(post.date), "LLLL d, yyyy")}
+                </time>
+              </div>
+            );
+          })}
+        </div>
+        <div className="md:w-[40%]">
+          <div className="sticky top-0">
+            <Chat />
           </div>
-        )
-      })}
-      </div>
-      <div className="md:w-[40%]">
-
+        </div>
       </div>
     </div>
-      
-
-    </div>
-
   );
 };
 
