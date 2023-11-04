caac1ab487e1ee46c8dcd21b65f48353d747f6b8
Abbas-Abdelila <abbasabdelila@gmail.com>
2023-11-04 21:13:02 +0300

    add close button

---
+++ components/CloseButton.tsx
@@ -0,0 +1,30 @@
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/BLPtHj4Anfk
+ */
+import { Button } from "@/components/ui/button"
+
+export default function Close() {
+  return (
+    <div className="mt-4 mb-8">
+      <Button aria-label="Close chatbot" variant="outline" >
+        <svg
+          className=" h-6 w-6"
+          fill="none"
+          height="24"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+          width="24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M18 6 6 18" />
+          <path d="m6 6 12 12" />
+        </svg>
+      </Button>
+    </div>
+  )
+}
+
