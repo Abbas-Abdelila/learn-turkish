cf7310f338bdb69a6094641559af1b9b38f44d7c
Abbas-Abdelila <abbasabdelila@gmail.com>
2023-11-04 16:21:41 +0300

    fix: open links in new tab

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -27,7 +27,7 @@ const Resources = () => {
         <p className="text-gray-800">
           • Yedi Iklim A1 Study Book{" "}
           <span className="text-red-500 font-semibold">
-            <Link href="https://cloudflare-ipfs.com/ipfs/bafykbzaceam7d4mjzwytixiiyjr4odbqurcdwpah6psgltof4llmlkkvtdtzy?filename=ibrahim%20gultekin%2C%20mahir%20kalfa%2C%20ibrahim%20atabey%20-%20Yedi%20%C4%B0klim%20T%C3%BCrk%C3%A7e%20A1%20Ders%20Kitab%C4%B1-Yunus%20Emre%20Enstit%C3%BCs%C3%BC%20%282015%29.pdf">
+            <Link target="_blank" href="https://cloudflare-ipfs.com/ipfs/bafykbzaceam7d4mjzwytixiiyjr4odbqurcdwpah6psgltof4llmlkkvtdtzy?filename=ibrahim%20gultekin%2C%20mahir%20kalfa%2C%20ibrahim%20atabey%20-%20Yedi%20%C4%B0klim%20T%C3%BCrk%C3%A7e%20A1%20Ders%20Kitab%C4%B1-Yunus%20Emre%20Enstit%C3%BCs%C3%BC%20%282015%29.pdf">
               [PDF]
             </Link>
           </span>
@@ -35,7 +35,7 @@ const Resources = () => {
         <p className="text-gray-800">
           • Yedi Iklim A2 Study Book{" "}
           <span className="text-red-500 font-semibold">
-            <Link href="https://cloudflare-ipfs.com/ipfs/bafykbzaceawdwgrsd4jnndqx4bo5hdxvl5nclk4nwbrzk4susfr2cje35lluk?filename=ibrahim%20gultekin%2C%20mahir%20kalfa%2C%20ibrahim%20atabey%20-%20Yedi%20%C4%B0klim%20T%C3%BCrk%C3%A7e%20A2%20Ders%20Kitab%C4%B1-Yunus%20Emre%20Enstit%C3%BCs%C3%BC%20%282015%29.pdf">
+            <Link target="_blank" href="https://cloudflare-ipfs.com/ipfs/bafykbzaceawdwgrsd4jnndqx4bo5hdxvl5nclk4nwbrzk4susfr2cje35lluk?filename=ibrahim%20gultekin%2C%20mahir%20kalfa%2C%20ibrahim%20atabey%20-%20Yedi%20%C4%B0klim%20T%C3%BCrk%C3%A7e%20A2%20Ders%20Kitab%C4%B1-Yunus%20Emre%20Enstit%C3%BCs%C3%BC%20%282015%29.pdf">
               [PDF]
             </Link>
           </span>
@@ -43,7 +43,7 @@ const Resources = () => {
         <p className="text-gray-800">
           • Yedi Iklim B1 Study Book{" "}
           <span className="text-red-500 font-semibold">
-            <Link href="https://cloudflare-ipfs.com/ipfs/bafykbzacecwbfdtfjwsm6s2vlfl6pvpbvebmqbtua7acu56zgfxdymljrjeyq?filename=ibrahim%20gultekin%2C%20mahir%20kalfa%2C%20ibrahim%20atabey%20-%20Yedi%20%C4%B0klim%20T%C3%BCrk%C3%A7e%20B1%20Ders%20Kitab%C4%B1-Yunus%20Emre%20Enstit%C3%BCs%C3%BC%20%282015%29.pdf">
+            <Link target="_blank" href="https://cloudflare-ipfs.com/ipfs/bafykbzacecwbfdtfjwsm6s2vlfl6pvpbvebmqbtua7acu56zgfxdymljrjeyq?filename=ibrahim%20gultekin%2C%20mahir%20kalfa%2C%20ibrahim%20atabey%20-%20Yedi%20%C4%B0klim%20T%C3%BCrk%C3%A7e%20B1%20Ders%20Kitab%C4%B1-Yunus%20Emre%20Enstit%C3%BCs%C3%BC%20%282015%29.pdf">
               [PDF]
             </Link>
           </span>
@@ -51,7 +51,7 @@ const Resources = () => {
         <p className="text-gray-800">
           • Yedi Iklim B2 Study Book{" "}
           <span className="text-red-500 font-semibold">
-            <Link href="https://cloudflare-ipfs.com/ipfs/bafykbzaceca3jhxelwrjodya6jxdj3i6gesjrfbz6koyaopnz72jyinzurnmy?filename=ibrahim%20gultekin%2C%20mahir%20kalfa%2C%20ibrahim%20atabey%20-%20Yedi%20%C4%B0klim%20T%C3%BCrk%C3%A7e%20B2%20Ders%20Kitab%C4%B1-Yunus%20Emre%20Enstit%C3%BCs%C3%BC%20%282016%29.pdf">
+            <Link target="_blank" href="https://cloudflare-ipfs.com/ipfs/bafykbzaceca3jhxelwrjodya6jxdj3i6gesjrfbz6koyaopnz72jyinzurnmy?filename=ibrahim%20gultekin%2C%20mahir%20kalfa%2C%20ibrahim%20atabey%20-%20Yedi%20%C4%B0klim%20T%C3%BCrk%C3%A7e%20B2%20Ders%20Kitab%C4%B1-Yunus%20Emre%20Enstit%C3%BCs%C3%BC%20%282016%29.pdf">
               [PDF]
             </Link>
           </span>
@@ -59,7 +59,7 @@ const Resources = () => {
         <p className="text-gray-800">
           • Yedi Iklim C1 Study Book{" "}
           <span className="text-red-500 font-semibold">
-            <Link href="https://cloudflare-ipfs.com/ipfs/bafykbzacedqhxle7qbu2i667ri6qefacbywwb2dj62jtdxpo6x3qairnq2pai?filename=ibrahim%20gultekin%2C%20mahir%20kalfa%2C%20ibrahim%20atabey%20-%20Yedi%20%C4%B0klim%20T%C3%BCrk%C3%A7e%20C1%20Ders%20Kitab%C4%B1-Yunus%20Emre%20Enstit%C3%BCs%C3%BC%20%282016%29.pdf">
+            <Link target="_blank" href="https://cloudflare-ipfs.com/ipfs/bafykbzacedqhxle7qbu2i667ri6qefacbywwb2dj62jtdxpo6x3qairnq2pai?filename=ibrahim%20gultekin%2C%20mahir%20kalfa%2C%20ibrahim%20atabey%20-%20Yedi%20%C4%B0klim%20T%C3%BCrk%C3%A7e%20C1%20Ders%20Kitab%C4%B1-Yunus%20Emre%20Enstit%C3%BCs%C3%BC%20%282016%29.pdf">
               [PDF]
             </Link>
           </span>
@@ -67,7 +67,7 @@ const Resources = () => {
         <p className="text-gray-800">
           • Yedi Iklim C2 Study Book{" "}
           <span className="text-red-500 font-semibold">
-            <Link href="https://cloudflare-ipfs.com/ipfs/bafykbzacecrpjkfbbersfpi3jq4eg3ws6tkoz7xtgy3qrdwsopwb6dn54477y?filename=ibrahim%20gultekin%2C%20mahir%20kalfa%2C%20ibrahim%20atabey%2C%20erol%20barin%20-%20Yedi%20%C4%B0klim%20T%C3%BCrk%C3%A7e%20C2%20Ders%20Kitab%C4%B1-Yunus%20Emre%20Enstit%C3%BCs%C3%BC%20%282016%29.pdf">
+            <Link target="_blank" href="https://cloudflare-ipfs.com/ipfs/bafykbzacecrpjkfbbersfpi3jq4eg3ws6tkoz7xtgy3qrdwsopwb6dn54477y?filename=ibrahim%20gultekin%2C%20mahir%20kalfa%2C%20ibrahim%20atabey%2C%20erol%20barin%20-%20Yedi%20%C4%B0klim%20T%C3%BCrk%C3%A7e%20C2%20Ders%20Kitab%C4%B1-Yunus%20Emre%20Enstit%C3%BCs%C3%BC%20%282016%29.pdf">
               [PDF]
             </Link>
           </span>
@@ -90,7 +90,7 @@ const Resources = () => {
         <p className="text-gray-800 md:w-[230px] ">
           • Istanbul Yabancılar için Türkçe <span className="font-medium text-red-500">A1</span> Study Book{" "}
           <span className="text-red-500 font-semibold">
-            <Link href="https://cloudflare-ipfs.com/ipfs/bafykbzaceapfah3tsr76lanjiwgly6h6guwmgnbqi6fkotb26tztxi24s3mc4?filename=%20-%20%C4%B0stanbul.%20Yabanc%C4%B1lar%20%C4%B0%C3%A7in%20T%C3%BCrk%C3%A7e%20Ders%20Kitab%C4%B1%20A1.pdf">
+            <Link target="_blank" href="https://cloudflare-ipfs.com/ipfs/bafykbzaceapfah3tsr76lanjiwgly6h6guwmgnbqi6fkotb26tztxi24s3mc4?filename=%20-%20%C4%B0stanbul.%20Yabanc%C4%B1lar%20%C4%B0%C3%A7in%20T%C3%BCrk%C3%A7e%20Ders%20Kitab%C4%B1%20A1.pdf">
               [PDF]
             </Link>
           </span>
@@ -98,7 +98,7 @@ const Resources = () => {
         <p className="text-gray-800 md:w-[230px]">
           • Istanbul Yabancılar için Türkçe <span className="font-medium text-red-500">A2</span> Study Book{" "}
           <span className="text-red-500 font-semibold">
-            <Link href="https://cloudflare-ipfs.com/ipfs/bafykbzaceawdwgrsd4jnndqx4bo5hdxvl5nclk4nwbrzk4susfr2cje35lluk?filename=ibrahim%20gultekin%2C%20mahir%20kalfa%2C%20ibrahim%20atabey%20-%20Yedi%20%C4%B0klim%20T%C3%BCrk%C3%A7e%20A2%20Ders%20Kitab%C4%B1-Yunus%20Emre%20Enstit%C3%BCs%C3%BC%20%282015%29.pdf">
+            <Link target="_blank" href="https://cloudflare-ipfs.com/ipfs/bafykbzaceawdwgrsd4jnndqx4bo5hdxvl5nclk4nwbrzk4susfr2cje35lluk?filename=ibrahim%20gultekin%2C%20mahir%20kalfa%2C%20ibrahim%20atabey%20-%20Yedi%20%C4%B0klim%20T%C3%BCrk%C3%A7e%20A2%20Ders%20Kitab%C4%B1-Yunus%20Emre%20Enstit%C3%BCs%C3%BC%20%282015%29.pdf">
               [PDF]
             </Link>
           </span>
@@ -106,7 +106,7 @@ const Resources = () => {
         <p className="text-gray-800 md:w-[230px]">
           • Istanbul Yabancılar için Türkçe <span className="font-medium text-red-500">B1</span> Study Book{" "}
           <span className="text-red-500 font-semibold">
-            <Link href="https://cloudflare-ipfs.com/ipfs/bafykbzacealgron6ogv55qlaa6lvewx6oangvmnwq5psnynhmqwyy4gr5rj3u?filename=%20-%20%C4%B0stanbul.%20Yabanc%C4%B1lar%20%C4%B0%C3%A7in%20T%C3%BCrk%C3%A7e%20B1_%20Ders%20Kitab%C4%B1.pdf">
+            <Link target="_blank" href="https://cloudflare-ipfs.com/ipfs/bafykbzacealgron6ogv55qlaa6lvewx6oangvmnwq5psnynhmqwyy4gr5rj3u?filename=%20-%20%C4%B0stanbul.%20Yabanc%C4%B1lar%20%C4%B0%C3%A7in%20T%C3%BCrk%C3%A7e%20B1_%20Ders%20Kitab%C4%B1.pdf">
               [PDF]
             </Link>
           </span>
