ba9a1eae0130f82e24964c2664e1605d52c9f3eb
Abbas-Abdelila <abbasabdelila@gmail.com>
2023-11-04 18:47:23 +0300

    fix: add link to post to img

--- components/Post.tsx
+++ components/Post.tsx
@@ -14,6 +14,7 @@ const PostComponent = ({ title, image, desc, url, level }: PostProps) => {
     <div className="flex w-[80%] mx-auto my-10 space-x-4 border-b py-5">
       {/* left */}
       <div className="relative w-12 h-12 sm:h-16 sm:w-24 md:h-24 md:w-32 lg:h-24 lg:w-30 hover:scale-105 transition transform duration-200 ease-in-out cursor-pointer">
+      <Link href={`/posts/${level.toLowerCase()}/${url}`}>
         <Image
           src={`/images/${image}`}
           alt="Level Image"
@@ -21,6 +22,7 @@ const PostComponent = ({ title, image, desc, url, level }: PostProps) => {
           sizes="512px"
           className="rounded-lg object-contain"
         />
+        </Link>
       </div>
 
       {/* right */}
