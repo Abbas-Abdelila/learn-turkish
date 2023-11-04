29922ff5e103762e724753078c5b9be47f100afa
Abbas-Abdelila <abbasabdelila@gmail.com>
2023-11-04 19:26:51 +0300

    feat: add new chat UI to all blogs

--- app/posts/[level]/[slug]/page.tsx
+++ app/posts/[level]/[slug]/page.tsx
@@ -52,7 +52,7 @@ const PostPage = ({ params }: { params: { slug: string } }) => {
   }
   const Component = getMDXComponent(currentPost.body.code);
   return (
-    <div className="flex w-[95%] mx-auto divide-x divide-red-200">
+    <div className="flex w-[95%] mx-auto">
     <div className="flex justify-center p-5 md:w-[60%]">
       <article className="prose prose-slate prose-xl max-w-3xl prose-hr:border-red-200 prose-strong:text-red-500 ">
         <Component components={{ ...ComponentMap }} />
