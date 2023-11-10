4bae181cc16634fd6be3f2482361aa7438b763d5
Abbas-Abdelila <abbasabdelila@gmail.com>
2023-11-11 02:12:35 +0300

    fix: image aspect ratio

--- components/Post.tsx
+++ components/Post.tsx
@@ -7,34 +7,32 @@ type PostProps = {
   desc: string;
   url: string;
   level: string;
-}
+};
 
 const PostComponent = ({ title, image, desc, url, level }: PostProps) => {
   return (
     <div className="flex w-[80%] mx-auto my-10 space-x-4 border-b py-5">
       {/* left */}
-      <div className="relative w-12 h-12 sm:h-16 sm:w-24 md:h-24 md:w-32 lg:h-24 lg:w-30 hover:scale-105 transition transform duration-200 ease-in-out cursor-pointer">
       <Link href={`/posts/${level.toLowerCase()}/${url}`}>
-        <Image
-          src={`/images/${image}`}
-          alt="Level Image"
-          fill
-          sizes="512px"
-          className="rounded-lg object-contain"
-        />
-        </Link>
-      </div>
+        <div className="relative w-12 h-12 sm:h-16 sm:w-24 md:h-24 md:w-32 lg:h-24 lg:w-30 hover:scale-105 transition transform duration-200 ease-in-out cursor-pointer">
+          <Image
+            src={`/images/${image}`}
+            alt="Level Image"
+            fill
+            sizes="512px"
+            className="rounded-lg object-contain"
+          />
+        </div>
+      </Link>
 
       {/* right */}
       <div className="flex flex-col w-full">
         <Link href={`/posts/${level.toLowerCase()}/${url}`}>
-        <h1 className="font-semibold text-xl text-gray-900 cursor-pointer line-clamp-2">
-          {title}
-        </h1>
+          <h1 className="font-semibold text-xl text-gray-900 cursor-pointer line-clamp-2">
+            {title}
+          </h1>
         </Link>
-        <p className="font-normal text-gray-600 text-md line-clamp-2">
-          {desc}
-        </p>
+        <p className="font-normal text-gray-600 text-md line-clamp-2">{desc}</p>
         <h3 className="text-right flex-1">A1</h3>
       </div>
     </div>
