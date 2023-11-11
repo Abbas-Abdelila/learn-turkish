fc0e8c12e36de2143a4f46fc49a9854e0987029c
Abbas-Abdelila <abbasabdelila@gmail.com>
2023-11-11 19:26:57 +0300

    Update sitemap URLs to include "www" prefix.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://pickturkish.com',
+      url: 'https://www.pickturkish.com',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
-      url: 'https://pickturkish.com/posts/all',
+      url: 'https://www.pickturkish.com/posts/all',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.8,
     },
     {
-      url: 'https://pickturkish.com/posts/a1',
+      url: 'https://www.pickturkish.com/posts/a1',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
