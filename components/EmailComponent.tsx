c2e0c6c4b0b04bbbb8ab390fea9602e19f9726a4
Abbas-Abdelila <abbasabdelila@gmail.com>
2023-11-11 17:03:17 +0300

    Add loading state and error handling to email submission form

--- components/EmailComponent.tsx
+++ components/EmailComponent.tsx
@@ -5,11 +5,16 @@ import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormField, FormItem } from "./ui/form";
+import { useState } from "react";
+import { useToast } from "./ui/use-toast";
 
 export default function EmailComponent() {
   const formSchema = z.object({
     email: z.string().email("Invalid Email") || z.string().min(3),
   });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const { toast } = useToast();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -20,17 +25,39 @@ export default function EmailComponent() {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     const { email } = values;
+    setIsLoading(true);
 
-    const response = await fetch("/api/email", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email,
-      })
-    })
+    try {
+      const response = await fetch("/api/email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+        }),
+      });
 
+      if (response.ok) {
+        toast({
+          description: "✨ Thanks for signing up!",
+          duration: 5000,
+        });
+        const json = await response.json();
+        console.log(json);
+      } else {
+        toast({
+          description: "Something went wrong.",
+          duration: 5000,
+        });
+        throw new Error("Something went wrong");
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+      form.reset();
+    }
   };
 
   return (
@@ -45,35 +72,37 @@ export default function EmailComponent() {
             </div>
             <div className="w-full max-w-sm space-y-2 mx-auto">
               <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="flex space-x-2"
-              >
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Input
-                          className="max-w-lg flex-1 bg-white text-black border-gray-900"
-                          placeholder="Enter your email"
-                          type="email"
-                          {...field}
-                        />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-
-                <Button
-                  className="bg-black text-white"
-                  variant="email"
-                  type="submit"
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="flex space-x-2"
                 >
-                  Get Notified
-                </Button>
-              </form>
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input
+                            className="max-w-lg flex-1 bg-white text-black border-gray-900"
+                            placeholder="Enter your email"
+                            type="email"
+                            {...field}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+
+                  <Button
+                    className="bg-black text-white"
+                    variant="email"
+                    type="submit"
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    onClick={form.handleSubmit(onSubmit)}
+                  >
+                    {isLoading ? "Sending..." : "Notify Me"}
+                  </Button>
+                </form>
               </Form>
             </div>
           </div>
