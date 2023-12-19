b16e66b4fc785882958fc7ddd25eeadf2443e2cb
Abbas-Abdelila <abbasabdelila@gmail.com>
2023-12-20 02:31:39 +0300

    Refactor Chat component form and add messageEndRef div

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -63,8 +63,8 @@ export default function Chat() {
               </h4>
             </div>
           )}
-          
-          <form onSubmit={handleSubmit} autoComplete="on">
+          <div ref={messageEndRef}></div>
+          <form className="my-12" onSubmit={handleSubmit} autoComplete="on">
             <div
               className={`flex items-center space-between fixed bottom-0 w-full max-w-md p-2 mb-8  ${
                 inputFocused
@@ -98,7 +98,6 @@ export default function Chat() {
               </button>
             </div>
           </form>
-          <div ref={messageEndRef}></div>
         </div>
       ) : (
         <div onClick={() => setIsChatOpen(true)}>
