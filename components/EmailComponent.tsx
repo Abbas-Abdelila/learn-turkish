cb54fffd4b55664697149cfeb76df20de28ab5f2
Abbas-Abdelila <abbasabdelila@gmail.com>
2024-01-04 15:47:09 +0300

    add dark mode styling to EmailComponent

--- components/EmailComponent.tsx
+++ components/EmailComponent.tsx
@@ -61,12 +61,12 @@ export default function EmailComponent() {
   };
 
   return (
-    <section className="py-3 md:py-6 lg:py-8 xl:py-12 bg-[#f1f5f8]">
+    <section className="py-3 md:py-6 lg:py-8 xl:py-12 bg-[#f1f5f8] dark:bg-[#262626]">
       <div className="container px-4 md:px-6">
         <div className="grid gap-6 items-center">
           <div className="flex flex-col justify-center space-y-4 text-center">
             <div className="space-y-2">
-              <h1 className="bg-gradient-to-r p-3 from-gray-600 via-gray-800 to-red-500 bg-clip-text text-lg font-extrabold text-transparent sm:text-2xl md:text-3xl lg:text-4xl leading-normal">
+              <h1 className="bg-gradient-to-r p-3 from-gray-600 via-gray-800 to-red-500 bg-clip-text text-lg font-extrabold text-transparent sm:text-2xl md:text-3xl lg:text-4xl dark:text-white leading-normal">
                 Discover new levels before anyone else!
               </h1>
             </div>
