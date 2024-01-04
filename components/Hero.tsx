07e866b68334eb3d4f570b61bea5a9d39fe46629
Abbas-Abdelila <abbasabdelila@gmail.com>
2024-01-04 15:48:49 +0300

    Update Hero component styling : dark mode

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,9 +4,9 @@ const Hero = () => {
   return (
     <div className="flex justify-between items-center bg-gradient-hero rounded-xl  md:rounded-2xl lg:rounded-3xl my-10 p-5 lg:p-8">
       <div>
-        <h1 className="bg-gradient-to-r p-3 from-gray-600 via-gray-800 to-red-500 bg-clip-text text-xl font-extrabold text-transparent sm:text-2xl md:text-3xl lg:text-5xl leading-normal">
+        <h2 className="bg-gradient-to-r p-3 from-gray-600 via-gray-800 to-red-500 bg-clip-text text-xl font-extrabold text-transparent sm:text-2xl md:text-3xl lg:text-5xl leading-normal dark:text-[#f5f5f5]">
           Your One-Stop Turkish Learning Place.
-        </h1>
+        </h2>
       </div>
       <div className="relative w-24 h-24 sm:h-32 sm:w-48 md:h-48 md:w-64 lg:h-48 lg:w-60 ">
         <Image
