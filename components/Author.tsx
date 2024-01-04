d81573a5c43541310964ff24db64b4bfa906bbd1
Abbas-Abdelila <abbasabdelila@gmail.com>
2024-01-04 15:46:14 +0300

    add dark mode styling

--- components/Author.tsx
+++ components/Author.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Author = () => {
   return (
-    <div className="flex items-center justify-between h-16 md:h-24 px-2 py-1  rounded-xl border border-gray-300 bg-[#f1f5f8]">
+    <div className="flex items-center justify-between h-16 md:h-24 px-2 py-1 rounded-xl border border-gray-300 bg-[#f1f5f8] dark:bg-[#262626]">
       <div className="flex items-center space-x-3">
         <Image
           src="/images/abbas.jpg"
@@ -13,7 +13,7 @@ const Author = () => {
           width={64}
           className="rounded-full h-10 w-10 md:h-16 md:w-16"
         />
-        <p className="font-semibold text-md md:text-lg text-gray-900">Abbas Abdelila</p>
+        <p className="font-semibold text-md md:text-lg text-gray-900 dark:text-white">Abbas Abdelila</p>
       </div>
 
       <div className="flex justify-center items-center space-x-4">
@@ -21,7 +21,7 @@ const Author = () => {
           <Icons.twitter />
         </Link>
         <Link href="https://twitter.com/abbasabdelila" target="_blank" className="no-underline hidden md:flex hover:font-semibold transition-transform duration-150 ease-in-out">
-          <p>@abbasabdelila</p>
+          <p className="dark:text-white">@abbasabdelila</p>
         </Link>
       </div>
     </div>
