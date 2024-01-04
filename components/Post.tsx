0a09515998d9c53af76cb47dbd3c746d4124fa1e
Abbas-Abdelila <abbasabdelila@gmail.com>
2024-01-04 15:56:24 +0300

    Update PostComponent styles : dark mode

--- components/Post.tsx
+++ components/Post.tsx
@@ -11,7 +11,7 @@ type PostProps = {
 
 const PostComponent = ({ title, image, desc, url, level }: PostProps) => {
   return (
-    <div className="flex w-[80%] mx-auto my-10 space-x-4 border-b py-5">
+    <div className="flex w-[80%] mx-auto my-10 space-x-4 border-b dark:border-white py-5">
       {/* left */}
       <Link href={`/posts/${level.toLowerCase()}/${url}`}>
         <div className="relative w-12 h-12 sm:h-16 sm:w-24 md:h-24 md:w-32 lg:h-24 lg:w-30 hover:scale-105 transition transform duration-200 ease-in-out cursor-pointer">
@@ -28,11 +28,11 @@ const PostComponent = ({ title, image, desc, url, level }: PostProps) => {
       {/* right */}
       <div className="flex flex-col w-full">
         <Link href={`/posts/${level.toLowerCase()}/${url}`}>
-          <h1 className="font-semibold text-xl text-gray-900 cursor-pointer line-clamp-2">
+          <h3 className="font-semibold text-xl text-gray-900 dark:text-white cursor-pointer line-clamp-2">
             {title}
-          </h1>
+          </h3>
         </Link>
-        <p className="font-normal text-gray-600 text-md line-clamp-2">{desc}</p>
+        <p className="font-normal text-gray-600 text-md line-clamp-2 dark:text-white">{desc}</p>
         <h3 className="text-right flex-1">A1</h3>
       </div>
     </div>
