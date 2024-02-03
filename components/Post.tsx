c99d46d28655f74c778bf7ae757500d64084fd87
Abbas-Abdelila <abbasabdelila@gmail.com>
2024-02-03 19:05:59 +0300

    style: rounded image

--- components/Post.tsx
+++ components/Post.tsx
@@ -14,12 +14,12 @@ const PostComponent = ({ title, image, desc, url, level }: PostProps) => {
     <div className="flex w-[80%] mx-auto my-10 space-x-4 border-b dark:border-white py-5">
       {/* left */}
       <Link href={`/posts/${level.toLowerCase()}/${url}`}>
-        <div className="relative w-12 h-12 sm:h-16 sm:w-24 md:h-24 md:w-32 lg:h-24 lg:w-30 hover:scale-105 transition transform duration-200 ease-in-out cursor-pointer">
+        <div className="relative w-12 h-12 sm:h-16 sm:w-16 md:h-24 md:w-24 lg:h-30 lg:w-30 hover:scale-105 transition transform duration-200 ease-in-out cursor-pointer">
           <Image
             src={`/images/${image}`}
             alt="Level Image"
             fill
-            sizes="512px"
+            sizes="(max-width: 600px) 100vw, 50vw"
             className="rounded-lg object-contain"
           />
         </div>
