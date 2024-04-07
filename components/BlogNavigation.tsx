a029ce2e6939f6fc30af4614106b744ff3fcfa96
Abbas-Abdelila <abbasabdelila@gmail.com>
2024-04-07 21:11:43 +0300

    fix: display level correctly in url

--- components/BlogNavigation.tsx
+++ components/BlogNavigation.tsx
@@ -2,9 +2,13 @@ import Link from "next/link";
 import { JSX, SVGProps } from "react";
 import { allPosts } from "@/.contentlayer/generated";
 
-export default function BlogNavigation( { currentPostIndex } : { currentPostIndex : number}) {
-
-  let previousPost = null, nextPost = null;
+export default function BlogNavigation({
+  currentPostIndex,
+}: {
+  currentPostIndex: number;
+}) {
+  let previousPost = null,
+    nextPost = null;
 
   if (currentPostIndex - 1 >= 0) {
     previousPost = allPosts[currentPostIndex - 1];
@@ -14,13 +18,24 @@ export default function BlogNavigation( { currentPostIndex } : { currentPostInde
     nextPost = allPosts[currentPostIndex + 1];
   }
 
-  
   return (
     <div className="flex justify-between gap-4 md:gap-8 items-center py-6 lg:py-16">
       <div className="flex flex-col">
-        <Link className={`group inline-flex align-center ${!previousPost ? "disabled opacity-40" : ""} `} href={previousPost?.url || "#"} passHref>
+        <Link
+          className={`group inline-flex align-center ${
+            !previousPost ? "disabled opacity-40" : ""
+          } `}
+          href={
+            previousPost?.url
+              ? `/posts/${previousPost.level.toLowerCase()}/${previousPost.url}`
+              : "#"
+          }
+          passHref
+        >
           <div className="flex items-center">
-            <span><IconArrowleft className="h-6 w-6 mr-2" /></span>
+            <span>
+              <IconArrowleft className="h-6 w-6 mr-2" />
+            </span>
             <p className="text-base sm:text-lg md:text-xl lg:text-2xl font-semibold text-zinc-600 dark:text-zinc-400">
               Previous Post
             </p>
@@ -31,17 +46,26 @@ export default function BlogNavigation( { currentPostIndex } : { currentPostInde
         </h2>
       </div>
       <div className="flex flex-col">
-        <Link className={`group inline-flex align-center ${!nextPost ? "disabled opacity-40" : ""} `} href={nextPost?.url || "#"}>
+        <Link
+          className={`group inline-flex align-center ${
+            !nextPost ? "disabled opacity-40" : ""
+          } `}
+          href={nextPost?.url
+            ? `/posts/${nextPost.level.toLowerCase()}/${nextPost.url}`
+            : "#"}
+        >
           <div className="flex items-center justify-end w-full">
             <p className="text-base sm:text-lg md:text-xl lg:text-2xl text-right font-semibold text-zinc-600 dark:text-zinc-400">
               Next Post
             </p>
-            <span><IconArrowright className="h-6 w-6 ml-2" /></span>
+            <span>
+              <IconArrowright className="h-6 w-6 ml-2" />
+            </span>
           </div>
         </Link>
-          <h2 className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold text-zinc-900 dark:text-zinc-200">
-            {nextPost?.title}
-          </h2>
+        <h2 className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold text-zinc-900 dark:text-zinc-200">
+          {nextPost?.title}
+        </h2>
       </div>
     </div>
   );
