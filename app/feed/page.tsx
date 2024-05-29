6391e29888ff60aa8d6139ae3ff45d91377e6b56
Abbas-Abdelila <abbasabdelila@gmail.com>
2024-05-29 21:31:57 +0300

    debug on prod  💀

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,13 +1,20 @@
-import { authOptions } from '@/lib/auth';
-import { getServerSession } from 'next-auth';
-import React from 'react'
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import React from "react";
+import CourseCard from "./CourseCard";
 
 const Feed = async () => {
   const session = await getServerSession(authOptions);
-    console.log("Session : ", session);
+  console.log("Session : ", session);
   return (
-    <div className='h-screen flex justify-center items-center text-2xl'>Feed me more {session?.user.username}</div>
-  )
-}
+    <>
+      <CourseCard />
+      <div className="h-screen flex justify-center items-center text-2xl">
+        Feed me more {session?.user.username} 🍔
+        UserID: {session?.user.id} 
+      </div>
+    </>
+  );
+};
 
-export default Feed
+export default Feed;
